4dd9de80a35afdca9ef2792639e3621cbbcf5c92
hayanmini <dudals9696@gmail.com>
2025-08-25 11:47:19 +0900

    SafeArea > MediaQuery 변경 완료

--- lib/features/movie_info/presentation/pages/home_page.dart
+++ lib/features/movie_info/presentation/pages/home_page.dart
@@ -11,35 +11,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final topPadding = MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Text("가장 인기있는"),
-              ),
-              SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: Container(
-                  width: double.infinity,
-                  height: 600,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.tealAccent,
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: topPadding),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Text("가장 인기있는"),
+            ),
+            SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Container(
+                width: double.infinity,
+                height: 600,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.tealAccent,
                 ),
               ),
-              SizedBox(height: 5),
-              MovieSection(title: "현재 상영중", isRank: false),
-              MovieSection(title: "인기순", isRank: true),
-              MovieSection(title: "평점 높은순", isRank: false),
-              MovieSection(title: "개봉 예정", isRank: false),
-            ],
-          ),
+            ),
+            SizedBox(height: 5),
+            MovieSection(title: "현재 상영중", isRank: false),
+            MovieSection(title: "인기순", isRank: true),
+            MovieSection(title: "평점 높은순", isRank: false),
+            MovieSection(title: "개봉 예정", isRank: false),
+          ],
         ),
       ),
     );
